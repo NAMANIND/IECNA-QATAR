3fe5fa26ab4ea18443911845c3d7905453b7c5a5
Naman rai <namanrai309@gmail.com>
2024-09-02 12:34:25 +0530

    io

--- src/components/mainpage/igate/Igate.js
+++ src/components/mainpage/igate/Igate.js
@@ -59,7 +59,7 @@ function Ig() {
                 className={`mt-7 text-7xl text-white leading-[90px] w-full max-md:text-4xl ${anton.className}`}
                 dateTime="2024-07-12"
               >
-                19<sup className="text-white ">th</sup> SEPT 2024
+                15<sup className="text-white ">th</sup> NOV 2024
               </time>
               <p className="mt-1 text-[#ccff00] uppercase text-7xl text-left leading-[117%]  sm:mb-0 mb-4  font-bold">
                 QATAR
@@ -94,7 +94,7 @@ function Ig() {
             className={`mt-7 text-5xl text-white leading-[90px]  max-md:text-4xl ${anton.className}`}
             dateTime="2024-07-12"
           >
-            19<sup className="text-white ">th</sup> SEPTEMBER 2024
+            15<sup className="text-white ">th</sup> November 2024
           </time>
           <p className="mt-5 text-[#ccff00] uppercase leading-[117%]  sm:mb-0 mb-4  font-bold">
             QATAR
--- src/components/mainpage/pastevent/Pastevents.js
+++ src/components/mainpage/pastevent/Pastevents.js
@@ -146,7 +146,7 @@ function Pastevents() {
                 <h2
                   className={`mt-5 text-2xl tracking-tighter  ${work_sans.className} `}
                 >
-                  19<sup>th</sup> September 2024
+                  15<sup>th</sup> November 2024
                 </h2>
               </div>
             </div>
